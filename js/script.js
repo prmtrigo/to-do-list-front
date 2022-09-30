1640e656ca060bc74ed5f4481f344a3d60696289
Paulo <trigopaulo@gmail.com>
2022-09-30 14:24:27 -0300

    javascript add

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-alert("Hello!")
+window.addEventListener('load',() => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        const task = input.value;
+
+        //creating the div element -> new task
+        const task_el = document.createElement('div');
+        task_el.classList.add('task');
+
+        //adding the content -> task content
+        const task_content_el = document.createElement('div');
+        task_content_el.classList.add('content');
+
+        task_el.appendChild(task_content_el);
+
+        
+    })
+})
